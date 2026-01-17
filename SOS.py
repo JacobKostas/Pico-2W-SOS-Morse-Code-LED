1ef7b8f57198dc6b89e81d4e88bfc2e820d075a4
Kostas <142234141+JacobKostas@users.noreply.github.com>
2026-01-17 15:51:47 +0200

    Add files via upload

---
+++ SOS.py
@@ -0,0 +1,48 @@
+from machine import Pin
+import time
+
+led = Pin("LED", Pin.OUT)
+
+while True:
+    led.value(1)
+    time.sleep(0.25)
+    led.value(0)
+    time.sleep(0.25)
+    led.value(1)
+    time.sleep(0.25)
+    led.value(0)
+    time.sleep(0.25)
+    led.value(1)
+    time.sleep(0.25)
+    led.value(0)
+    time.sleep(0.25)
+    
+    led.value(1)
+    time.sleep(0.5)
+    led.value(0)
+    time.sleep(0.5)
+    led.value(1)
+    time.sleep(0.5)
+    led.value(0)
+    time.sleep(0.5)
+    led.value(1)
+    time.sleep(0.5)
+    led.value(0)
+    time.sleep(0.5)
+    
+    led.value(1)
+    time.sleep(0.25)
+    led.value(0)
+    time.sleep(0.25)
+    led.value(1)
+    time.sleep(0.25)
+    led.value(0)
+    time.sleep(0.25)
+    led.value(1)
+    time.sleep(0.25)
+    led.value(0)
+    time.sleep(0.25)
+    
+    led.value(0)
+    time.sleep(3)
+    
